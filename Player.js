d959066b210a96ea944c7226757075ff681388c3
stefanluks <stefan.luks@rede.ulbra.br>
2022-10-10 09:03:52 -0300

    add walk animation p2

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 export class Player {
 
-    constructor({ sprite, posicao, velocidade, dimensao, cor = "red", player2 = false, animacao, animacaoPulo }) {
+    constructor({ sprite, posicao, velocidade, dimensao, cor = "red", player2 = false, animacao }) {
         this.debug = false;
         this.sprite = sprite;
         this.posicao = posicao;
@@ -39,7 +39,6 @@ export class Player {
         ]
         this.animSet = 0;
         this.anima = animacao;
-        this.animaJump = animacaoPulo;
     }
 
     Atualizar(quadro, gravidade, ctx) {
@@ -71,54 +70,52 @@ export class Player {
             ctx.fillStyle = this.cor;
             ctx.fillRect(this.posicao.x, this.posicao.y, this.dimensao.w, this.dimensao.h);
         }
-        if (true) {
-            if (this.sprite) {
-                if (this.atacando) {
+        if (this.sprite) {
+            if (this.atacando) {
 
-                    let ataque = this.Ataques[this.ataqueSelecionado];
+                let ataque = this.Ataques[this.ataqueSelecionado];
 
-                    if (this.debug) {
-                        ctx.fillStyle = ataque.cor;
-                        ctx.fillRect(
-                            ataque.posicao.x,
-                            ataque.posicao.y,
-                            ataque.caixa.w,
-                            ataque.caixa.h
-                        )
+                if (this.debug) {
+                    ctx.fillStyle = ataque.cor;
+                    ctx.fillRect(
+                        ataque.posicao.x,
+                        ataque.posicao.y,
+                        ataque.caixa.w,
+                        ataque.caixa.h
+                    )
+                }
+
+                if (this.player2) {
+                    if (this.ataqueSelecionado == 0) {
+                        ctx.drawImage(this.sprite, 833, 500, 100, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
                     }
-
-                    if (this.player2) {
-                        if (this.ataqueSelecionado == 0) {
-                            ctx.drawImage(this.sprite, 833, 500, 100, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
-                        }
-                        if (this.ataqueSelecionado == 1) {
-                            ctx.drawImage(this.sprite, 515, 500, 100, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
-                        }
-                    } else {
-                        if (this.ataqueSelecionado == 0) {
-                            ctx.drawImage(this.sprite, 0, 500, 100, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
-                        }
-                        if (this.ataqueSelecionado == 1) {
-                            ctx.drawImage(this.sprite, 300, 500, 100, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
-                        }
+                    if (this.ataqueSelecionado == 1) {
+                        ctx.drawImage(this.sprite, 515, 500, 100, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
                     }
                 } else {
-                    if (this.andando) {
-                        if (this.player2) {
-                            ctx.drawImage(this.sprite, 0, 130, 100, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
-                        } else {
-                            if (this.frames % 5 == 0) {
-                                if (this.animSet < this.anima.length - 1) this.animSet++;
-                                else this.animSet = 0;
-                            }
-                            ctx.drawImage(this.sprite, this.anima[this.animSet].x, this.anima[this.animSet].y, 105, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
-                        }
+                    if (this.ataqueSelecionado == 0) {
+                        ctx.drawImage(this.sprite, 0, 500, 100, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
+                    }
+                    if (this.ataqueSelecionado == 1) {
+                        ctx.drawImage(this.sprite, 300, 500, 100, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
+                    }
+                }
+            } else {
+                if (this.andando) {
+                    if (this.frames % 5 == 0) {
+                        if (this.animSet < this.anima.length - 1) this.animSet++;
+                        else this.animSet = 0;
+                    }
+                    if (this.player2) {
+                        ctx.drawImage(this.sprite, this.anima[this.animSet].x, this.anima[this.animSet].y, 105, 120, this.posicao.x - 105, this.posicao.y, 200, 240);
                     } else {
-                        if (this.player2) {
-                            ctx.drawImage(this.sprite, 833, 0, 100, 130, this.posicao.x - 105, this.posicao.y, 200, 240);
-                        } else {
-                            ctx.drawImage(this.sprite, 0, 0, 100, 130, this.posicao.x + 5, this.posicao.y, 200, 240);
-                        }
+                        ctx.drawImage(this.sprite, this.anima[this.animSet].x, this.anima[this.animSet].y, 105, 120, this.posicao.x + 5, this.posicao.y, 200, 240);
+                    }
+                } else {
+                    if (this.player2) {
+                        ctx.drawImage(this.sprite, 833, 0, 100, 130, this.posicao.x - 105, this.posicao.y, 200, 240);
+                    } else {
+                        ctx.drawImage(this.sprite, 0, 0, 100, 130, this.posicao.x + 5, this.posicao.y, 200, 240);
                     }
                 }
             }
